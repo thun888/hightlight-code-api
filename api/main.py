4aa95376fd02fb533415ec52ca5bba22514f713f
thun888 <thun888@qq.com>
2025-03-09 21:13:35 +0800

    fix

--- api/main.py
+++ api/main.py
@@ -16,7 +16,7 @@ app = FastAPI(docs_url=None, redoc_url=None)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=str(True),
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -63,3 +63,4 @@ def index():
 
 if __name__ == "__main__":
         uvicorn.run("main:app", host="0.0.0.0", reload=True)
+        # uvicorn.run("main:app", host="0.0.0.0", reload=True,port=18081)
